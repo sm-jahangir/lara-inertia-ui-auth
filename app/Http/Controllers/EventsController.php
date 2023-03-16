223cbe01dac2f61ec4a3194913f7f4e558554a31
Jahangir Alam <samraatjahangir@gmail.com>
2023-03-16 10:15:24 +0600

    inertia server side done

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventsController extends Controller
+{
+    public function show(Event $event)
+    {
+        return Inertia::render('Event/Show', [
+            'event' => $event->only(
+                'id',
+                'title',
+                'start_date',
+                'description'
+            ),
+        ]);
+    }
+}
